ce76630ee136b150dae7acc7f1cea49a6cf10a42
Henry <42591018+HenryInkguy@users.noreply.github.com>
2024-09-13 19:02:13 -0300

    #

---
+++ 006class/A.py
@@ -0,0 +1,11 @@
+n1 = int(input('Type a number: '))
+n2 = int(input('Type a number: '))
+s = n1 + n2
+# print ('The sum is:', s)
+
+# int = 7, -4, 0, 9875, etc...
+# float = 4.5, 0.076, -15.223, 7.0, etc...
+# bool = True, False
+# str = 'Hello world!', '7.5', '', etc...
+
+print('The sum is: {}'.format(s))
---
+++ 006class/B.py
@@ -0,0 +1,4 @@
+n1 = int(input('Type a number: '))
+n2 = int(input('Another one: '))
+sum1 = n1 + n2
+print(f'The sum result of {n1} plus {n2} is: {sum1}')
