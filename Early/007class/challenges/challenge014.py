ebf730275477655f389dbfab45b28dd8ee9526d4
HenryInkguy <henrszcarmona@outlook.com>
2024-09-15 00:56:54 -0300

    # Lesson 7

--- Early/007class/challenges/challenge014.py
+++ Early/007class/challenges/challenge014.py
@@ -1,2 +1,23 @@
 # Challenge 014
-# 
+# Make a script that reads certain temperature in Celsius (C°) and then show the equivalent in Fahrenheit (F°). Vice-Verse for further challenge.
+
+print("1(Celsius to Fahrenheit) or 2(Fahrenheit to Celsius) converter, please type '1' for the first option or type '2' for the second one: ", end='')
+c_or_f = int(input(''))
+
+print('\n')
+
+if (c_or_f == 1):
+    print("Great, now type the temperature in Celsius you want to convert to Fahrenheit: ", end='')
+    c1 = float(input(''))
+    f1 = c1 * (9/5) + 32
+    print(f'\nCelsius: {c1:.2f} C°\nin\nFahrenheit: {f1:.2f} F°')
+
+elif (c_or_f == 2):
+    print("Great, now type the temperature in Fahrenheit you want to convert to Celsius: ", end='')
+    f1 = float(input(''))
+    c1 = (f1 - 32) * (5/9)
+    print(f'\nFahrenheit: {f1:.2f} F°\nin\nCelsius: {c1:.2f} C°')
+
+else:
+    print("Well... this ain't a valid number, try again.")
+
--- Early/007class/challenges/challenge015.py
+++ Early/007class/challenges/challenge015.py
@@ -1,2 +1,15 @@
 # Challenge 015
-# 
+# Make a script that reads the Kilometer ran by a rental car and the amount of days it was rented. Calculating the price to pay, considering that the car costs U$ 60 per day and U$ 0,15 per Km traveled.
+
+print("I see here you rented a car... right, let's see, the price you need to pay is... well, I don't have access to the records right now",end='')
+print("perhaps you could help me there, buddy? How many kilometers did you travel?")
+distance = float(input(''))
+print("Cool, now, how many days did you rent the car for?")
+duration = int(input(''))
+
+dur_total = duration*60 
+dis_total = distance*0.15
+
+total_val = dur_total + dis_total
+
+print(f"Right, staying with it for {duration} days and traveling around {distance}km, you need to pay me U$ {total_val:.2f}. Thank you, sir.")
