042aeb2f09cbcf274478f4d2963258c3cf69db17
HenryInkguy <henrszcarmona@outlook.com>
2024-09-15 01:29:36 -0300

    #

---
+++ Early/008class/A.py
@@ -0,0 +1,14 @@
+import math # import module (To import modules and libraries.)
+math.sqrt # module.function (To use a specific function of the module.)
+a = math.sqrt(50) # Example of using sqrt
+
+from math import sqrt # from module import function (To import only specific functions from a module.)
+
+# Examples
+
+math.ceil # Ceiling rounding (Rounding for the higher number. Ex: 5.5 rounds to 6)
+math.floor # Floor rounding (Rounding for the lower number. Ex: 5.5 rounds to 5)
+math.trunc # Truncate, eliminates all the numbers after a "." (Ex: 5.23874892174 truncates to 5. Without rounding at all.)
+math.pow # Power, exponentiation, basic stuff.
+math.sqrt # Square root, you definitely knows what that means.
+math.factorial # Factorial, I guess you know that.
