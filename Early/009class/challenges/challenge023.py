5c583ba253078281d5378e450fc55c200bbdfbfc
HenryInkguy <henrszcarmona@outlook.com>
2024-09-15 22:53:55 -0300

    # Another change in Lesson 9 c023

--- Early/009class/challenges/challenge023.py
+++ Early/009class/challenges/challenge023.py
@@ -1,2 +1,10 @@
 # Challenge 023
-# 
+# Make a script that reads a number between 0 and 9999 and shows and shows each one of the digits separated.
+
+number = str(input("Oh, hey, another project we are doing. Then, type a number between 0 and 9999, I will show you each digit of it.\n"))
+
+print(f"""The number you chose is {number}, good, now I will show you each digit:
+Unit: {number [3]}
+Tens: {number [2]}
+Hundreds: {number [1]}
+Thousands: {number [0]}""")
