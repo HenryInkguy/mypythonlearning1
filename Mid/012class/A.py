fee05da7acac3cfdbd5f44b7399ade4621390cfc
HenryInkguy <henrszcarmona@outlook.com>
2024-09-18 20:20:06 -0300

    #

---
+++ Mid/012class/A.py
@@ -0,0 +1,16 @@
+# If... Else... Elif.
+
+a = (1 + 3) * int(input(""))
+b = (2 + 2) * int(input(""))
+
+if a > b:
+    print("A > B")
+
+
+
+elif a < b:
+    print("A < B")
+
+
+else:
+    print("They're the same")
